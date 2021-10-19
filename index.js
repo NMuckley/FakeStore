59a3a17a4d44173b6df63dab99ea3a194906830a
NMuckley <92005577+NMuckley@users.noreply.github.com>
2021-10-18 20:43:49 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,45 +1,108 @@
 
 class Product {
-  constructor(name, price) {
+  constructor(name, price, handler) {
     this.name = name
     this.price = price
+    this.handler = handler
   }
 
   toHTML() {
-    return ` 
+    return `
     <div class="item">
       <p>${this.name}</p>
       <p class="price">$${this.price}</p>
-      <button>add to cart</button>
+      <button id="${this.nameToID()}">add to cart</button>
     </div>
     `
   }
+    
+  toCartHTML() {
+    return `
+    <div class="item">
+      <p>${this.name}</p>
+      <p class="price">$${this.price}</p>
+    </div>
+    `
+  }
+  
+  nameToID() {
+    return this.name.toLowerCase().replaceAll(" ", "-")
+  }
+  
+  addClickHandler() {
+    document.getElementById(this.nameToID()).addEventListener("click", () => this.handler(this.name, this.price) )
+  }
+  
 }
+
+
+class Cart {
+  constructor() {
+    this.products = []
+  }
+  
+  addProduct(product) {
+    this.products.push(product)
+    this.render()
+    
+  }
+
+  render() {
+    let productsHtml = this.products.map( product => product.toCartHTML())
+    const productsContainer = document.querySelector("#cartitems")
+    const productsHtmlString = productsHtml.join(" ")
+    productsContainer.innerHTML = productsHtmlString
+    
+    const totalCostContainer = document.querySelector("#totalcost")
+    totalCostContainer.innerHTML = `$${this.totalCost()}`
+  }
+  
+  totalCost() {
+    let productPrices = this.products.map( product => product.price)
+    return productPrices.reduce((total, num) => { return total + num }, 0)
+  }
+
+}
+
+
+
 
 class Shop {
   constructor() {
     this.fetchProducts()
+    this.cart = new Cart()
   }
 
   fetchProducts() {
     fetch("https://nate-shop.herokuapp.com/products", {
-      method: "get", 
+      method: "get",
       headers: {
         'Content-Type': 'application/json'
       }
     })
       .then( response => response.json())
       .then( data => {
-        console.log("data: ", data)
-        this.products = data.map( product => new Product(product.name, product.price))
+        this.products = data.map( product => new Product(product.name, product.price, this.addToCartHandler.bind(this)))
         this.render()
       })
   }
 
   render() {
     let productsHtml = this.products.map( product => product.toHTML())
-    document.querySelector("#products-list").innerHTML = productsHtml.join(" ")
+    const productsContainer = document.querySelector("#products-list")
+    const productsHtmlString = productsHtml.join(" ")
+    productsContainer.innerHTML = productsHtmlString
+    this.addProductHandlers()
+  }
+  
+  addProductHandlers() {
+    this.products.forEach( product => product.addClickHandler() )
+  }
+  
+  addToCartHandler(name, price) {
+    this.cart.addProduct(new Product(name, price, () => {} ))
   }
 }
 
 const shop = new Shop()
+
